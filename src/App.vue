5f3a616b288e440d2c3e820b6328487411de7f12
alkautsarm <alkautsarm@gmail.com>
2021-10-03 09:36:07 +0700

    feat: add initialization of stylus in project

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,5 @@
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
+@import "./styles/index.styl"
 </style>
