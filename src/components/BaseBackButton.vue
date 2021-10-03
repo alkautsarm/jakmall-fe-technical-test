662c1e610abc22f46cc1f50c37f70a12828fff14
alkautsarm <alkautsarm@gmail.com>
2021-10-03 21:56:41 +0700

    feat: add back button component

---
+++ src/components/BaseBackButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <router-link class="backButton" :to="path">
+    <img
+      src="@/assets/icons/arrow_back.svg"
+      alt="Navigation back arrow"
+    >
+
+    <span class="backButton__text">
+      {{ text }}
+    </span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'BaseBackButton',
+  props: {
+    text: {
+      type: String,
+      default: 'Back'
+    },
+    path: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.backButton
+  display: flex
+  align-items: center
+
+.backButton__text
+  opacity: 0.6
+  margin-left: 0.5rem
+</style>
