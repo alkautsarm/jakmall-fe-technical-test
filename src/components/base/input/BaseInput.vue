fe811523c5039913feaad82483ea19616dbe357e
alkautsarm <alkautsarm@gmail.com>
2021-10-06 03:36:57 +0700

    feat: add base input component

---
+++ src/components/base/input/BaseInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="form__inputGroup"
+    :class="model ? 'form__inputGroup--filled' : 'form__inputGroup--empty'"
+  >
+    <label v-if="model" class="form__inputLabel">
+      {{ inputLabel }}
+    </label>
+    <input
+      v-model="model"
+      @change="changeValue"
+      :type="inputType"
+      :placeholder="inputPlaceholder"
+      class="form__input"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    inputLabel: {
+      type: String,
+      required: true
+    },
+    inputPlaceholder: {
+      type: String,
+      required: true
+    },
+    inputModel: {
+      type: String,
+      required: true
+    },
+    inputType: {
+      type: String,
+      default: 'text'
+    },
+    formKey: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      model: this.inputModel
+    }
+  },
+  watch: {
+    inputModel (newValue) {
+      this.model = newValue
+    }
+  },
+  methods: {
+    changeValue () {
+      const data = {
+        formValue: this.model,
+        formKey: this.formKey
+      }
+      this.$emit('updateValue', data)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped></style>
