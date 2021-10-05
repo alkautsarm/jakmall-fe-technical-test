e9392cd2e15e7106cfae3e43f76650dbcd7e2c1b
alkautsarm <alkautsarm@gmail.com>
2021-10-05 14:07:31 +0700

    feat: add cart layout that can be reused

---
+++ src/layouts/CartLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <payment-navigation></payment-navigation>
+    <section class="section section--whiteBackground">
+      <base-back-button
+        v-if="backButtonText && backButtonPath"
+        text="Back to Cart"
+        path="/cart"
+      ></base-back-button>
+
+      <div class="mainContent">
+        <slot name="left" class="mainContent__detail"></slot>
+
+        <slot name="right"></slot>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import BaseBackButton from '@/components/BaseBackButton.vue'
+import PaymentNavigation from '@/components/payment/PaymentNavigation.vue'
+
+export default {
+  components: {
+    BaseBackButton,
+    PaymentNavigation
+  },
+  props: {
+    backButtonText: {
+      type: String,
+      default: null
+    },
+    backButtonPath: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~@/styles/index.styl"
+
+.mainContent
+  margin-top: 1.5rem
+  display: grid
+  grid-template-columns: auto auto auto auto auto
+  grid-gap: 2rem
+
+.mainContent__detail
+  grid-column-start: 1
+  grid-column-end: 5
+</style>
