2dea4f6716ca9ae0fd05c56e5286f1f4a0c9e1a4
alkautsarm <alkautsarm@gmail.com>
2021-10-07 08:22:17 +0700

    feat: create new home apge

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,27 @@
 <template>
-  <div>
-    <payment-navigation></payment-navigation>
-    <h1 class="heading">Hello World</h1>
-    <h2 class="heading">Hello World</h2>
-    <h3 class="heading">Hello World</h3>
-    <h4 class="heading">Hello World</h4>
-    <h5 class="heading">Hello World</h5>
-    <h6 class="heading">Hello World</h6>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a dapibus orci. Sed in lobortis urna. Quisque vestibulum, lectus nec malesuada venenatis, risus dolor scelerisque eros, in interdum purus eros id enim. Nullam sollicitudin, mauris sed euismod vulputate, tortor orci dapibus leo, a elementum nulla nulla vel augue. Maecenas accumsan eleifend nisi vel semper. In gravida, urna quis elementum porta, velit lectus pellentesque erat, a imperdiet purus arcu iaculis tortor. Duis eleifend metus id velit viverra, non finibus mauris consequat. Vestibulum eu magna venenatis augue dapibus iaculis. In consectetur quam sed purus facilisis molestie. Cras sagittis felis non tincidunt pretium. Aenean tristique enim ut ipsum luctus, at congue nisl dapibus. Maecenas id vestibulum enim. Praesent ut arcu nisl. Nam egestas orci non erat cursus, eget congue mauris aliquet.
-
-      Vivamus ut porttitor orci. Ut consectetur malesuada nibh, nec pharetra lectus scelerisque et. Nunc non tincidunt nisl. Vivamus vehicula justo nec quam gravida mollis. Etiam non erat luctus, imperdiet erat nec, facilisis nisl. Duis pretium eu quam et molestie. Sed molestie auctor volutpat. Nam euismod quam metus, blandit vulputate lorem lacinia eget. Aenean ut egestas nisl.
-    </p>
-    <button class="btn btn--primary">Continue to Payment</button>
-
-    <form action="" class="form">
-      <div class="form__inputGroup">
-        <input type="text" placeholder="Name" class="form__input">
-      </div>
-    </form>
-  </div>
+  <main class="home">
+    <h1 class="heading">Hello, Welcome to Jakmall FE Technical Test!</h1>
+    <router-link class="btn btn--primary home__button" to="/cart/delivery">
+      Start to Evaluate
+    </router-link>
+  </main>
 </template>
 
 <script>
-import PaymentNavigation from '../components/payment/PaymentNavigation.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    PaymentNavigation
-  }
+  name: 'Home'
 }
 </script>
+
+<style lang="stylus" scoped>
+.home
+  display: flex
+  align-items: center
+  justify-content: center
+  flex-direction: column
+  width: 100%
+  height: 70vh
+
+.home__button
+  margin-top: 1rem
+</style>
